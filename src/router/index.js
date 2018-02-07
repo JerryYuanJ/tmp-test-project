86753565409a242e482d178c4c4a37dad8f968e9
JerryYuanJ <610819167@qq.com>
2018-02-07 10:27:50 +0800

    路由再探

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,14 @@ import Page2 from '@/components/Page2'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+    // return 期望滚动到哪个的位置,前提是这个界面要能滚动，否则没有反应
+    return {
+      x:0,y:20
+    }
+  },
   routes: [
     {
       path: '/',
@@ -21,12 +28,17 @@ export default new Router({
     {
       path: '/richText',
       name: 'RichTextPage',
-      component: RichTextPage
+      component: RichTextPage,
+      beforeEnter(to, from, next){
+        console.info('enter rich text page');
+        next();
+      }
     },
     {
       path: '/table',
       name: 'DataTable',
-      component: DataTable
+      component: DataTable,
+      meta: {subject: 'java'}
     },
     {
       path: '/subRouter',
@@ -49,4 +61,20 @@ export default new Router({
       component: TabModal
     }
   ]
+});
+//全局守卫导航,next用来放行
+router.beforeEach((to, from, next) => {
+  console.info('global beforeEach');
+  //一个路由匹配到的所有路由记录会暴露为 $route 对象（还有在导航守卫中的路由对象）的 $route.matched 数组。
+  to.matched.some(record => {
+    if(record.meta.subject){
+      console.info('meta info :'+record.meta.subject)
+    }
+  })
+  next()
+});
+//全局后置钩子，不会改变导航的状态
+router.afterEach((to, from) => {
+  console.info('global afterEach');
 })
+export default router;
